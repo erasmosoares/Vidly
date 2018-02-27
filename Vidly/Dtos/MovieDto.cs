dcf3e52b5cef4f853a83d0de3480d8b9d9175cc3
Erasmo Soares de Araújo Júnior <erasmosaraujo@gmail.com>
2018-02-27 20:17:29 -0300

    Movies Api

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
+
+        [Required(ErrorMessage = "The Release Date field is required.")]
+        public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        //[MinNumberOfMoviesInStock]
+        [Required]
+        [Display(Name = "Number in Stock")]
+        public byte NumberInStock { get; set; }
+    }
+}
